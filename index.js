5319916e77d1aee68c1a6e1103521ca7b5425e86
Harpreet Singh <78143203+singhWeb@users.noreply.github.com>
2023-05-10 18:10:51 +0530

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-function updateText() {
-  var varCode = document.getElementById('varCode').value;
-  var varNumber = document.getElementById('varNumber').value;
-  var lnk = document.getElementById('lnk');
-  lnk.href = "https://api.whatsapp.com/send/?phone=%2B" + varCode + varNumber + "&text&type=phone_number&app_absent=0";
-  lnk.innerHTML = lnk.href;
-}
